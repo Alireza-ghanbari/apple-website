008753a0187631d2381993d0b0db794d86258395
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-09-08 00:01:18 +0430

    fixing

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -35,28 +35,19 @@ export default function Features() {
   return (
     <section className="h-full common-padding bg-zinc relative overflow-hidden">
       <div className="screen-max-width">
-        <div className="mt-12 w-full">
-          <h1 className="section-heading " id="features_title">
-            Explore the full story.
-          </h1>
+        <div className="mb-12 w-full">
+          <h1 id="features_title" className="section-heading">Explore the full story.</h1>
         </div>
+        
         <div className="flex flex-col justify-center items-center overflow-hidden">
-          <div className="mt-32 mb-24 pl-24">
+          <div className="mt-32 mb-24 pl-24 text-center">
             <h2 className="text-5xl lg:text-7xl font-semibold">iPhone.</h2>
-            <h2 className="text-5xl lg:text-7xl font-semibold">
-              Forged in titanium
-            </h2>
+            <h2 className="text-5xl lg:text-7xl font-semibold">Forged in titanium.</h2>
           </div>
+
           <div className="flex-center flex-col sm:px-10">
             <div className="relative h-[50vh] w-full flex items-center">
-              <video
-                playsInline
-                id="exploreVideo"
-                className="w-full h-full object-cover object-center"
-                muted
-                autoPlay
-                ref={videoRef}
-              >
+              <video playsInline id="exploreVideo" className="w-full h-full object-cover object-center" preload="none" muted autoPlay ref={videoRef}>
                 <source src={exploreVideo} type="video/mp4" />
               </video>
             </div>
@@ -64,43 +55,35 @@ export default function Features() {
             <div className="flex flex-col w-full relative">
               <div className="feature-video-container">
                 <div className="overflow-hidden flex-1 h-[50vh]">
-                  <img
-                    src={explore1Img}
-                    alt="titanium"
-                    className="feature-video g_grow"
-                  />
+                  <img src={explore1Img} alt="titanium" className="feature-video g_grow" />
                 </div>
                 <div className="overflow-hidden flex-1 h-[50vh]">
-                  <img
-                    src={explore2Img}
-                    alt="titaniu 2"
-                    className="feature-video g_grow"
-                  />
+                  <img src={explore2Img} alt="titanium 2" className="feature-video g_grow" />
                 </div>
               </div>
 
               <div className="feature-text-container">
                 <div className="flex-1 flex-center">
                   <p className="feature-text g_text">
-                    iPhone 15 pro is{" "}
+                    iPhone 15 Pro is {' '}
                     <span className="text-white">
-                      the first iphone to feature to an aerospace-grade titanium
-                      design
-                    </span>
-                    , using the same alloy that spacecraft use for mission to
-                    Mars.
+                      the first iPhone to feature an aerospace-grade titanium design
+                    </span>,
+                    using the same alloy that spacecrafts use for missions to Mars.
                   </p>
                 </div>
+
                 <div className="flex-1 flex-center">
                   <p className="feature-text g_text">
-                    Titanium has one of the best strength-to-weight ratios of
-                    any metal, making these our{" "}
+                    Titanium has one of the best strength-to-weight ratios of any metal, making these our {' '}
                     <span className="text-white">
                       lightest Pro models ever.
                     </span>
-                    You'll notice the diffrence the moment you pick one up.
+                    You'll notice the difference the moment you pick one up.
                   </p>
                 </div>
+
+
               </div>
             </div>
           </div>
