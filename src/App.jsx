cdc8d0b4f6613d256b0da4f3877125f9e7a9aac6
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-09-07 23:31:00 +0430

    complete footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Features from "./Components/Features";
+import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 import Highlights from "./Components/Highlights";
+import HowItWorks from "./Components/HowItWorks";
 import Model from "./Components/Model";
 import Navbar from "./Components/Navbar";
 
@@ -12,6 +14,8 @@ export default function App() {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   );
 }
